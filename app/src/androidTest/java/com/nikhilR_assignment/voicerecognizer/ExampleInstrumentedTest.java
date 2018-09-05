ccc83c4c0332621e056dee378a0d625984d2454b
nikkkkhil <nikkkkhil@yahoo.com>
2018-09-05 09:59:58 +0530

    second commit

--- app/src/androidTest/java/com/nikhilR_assignment/voicerecognizer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nikhilR_assignment/voicerecognizer/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
 		// Context of the app under test.
 		Context appContext = InstrumentationRegistry.getTargetContext();
 
-		assertEquals("com.sriyanksiddhartha.speechtotext", appContext.getPackageName());
+		assertEquals("nikhilR_assignment.voicerecognizer", appContext.getPackageName());
 	}
 }
